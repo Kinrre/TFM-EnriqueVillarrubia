8d924c50fe1fa781a081d746a98db99fc7556c3c
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2022-07-02 14:05:04 +0200

    fix: remove backend and frontend

--- backend/crud/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import auth_user
-from .game import create_user_game, delete_game, get_games, get_games_current_user, get_game_by_id, get_game_by_name_and_user, update_game, update_current_user_game
-from .match import create_match, get_match_by_id, get_match_by_room_code
-from .movement import create_movements
-from .piece import create_pieces
-from .user import create_user, delete_user, get_user_by_id, get_user_by_username, update_password
--- backend/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import Token, TokenData
-from .movement import Movement, MovementBase, MovementCreate
-from .piece import Piece, PieceBase, PieceCreate
-from .game import Game, GameCreate, GameUpdate
-from .user import User, UserCreate
-from .match import Match, MatchCreate
